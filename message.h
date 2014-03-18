7ffc2ca7782cb11fd7a632631a14964363dfbf39
oubeichen <oubeichen@gmail.com>
2014-03-18 18:58:13 +0800

    Message.h: serveral adjustment

--- message.h
+++ message.h
@@ -5,29 +5,30 @@
 #ifndef __MESSAGE_H__
 #define __MESSAGE_H__
 
-#define MAX_CONTENT_LENGTH 255
+#define MSG_MAX_CONTENT_LENGTH 255
+#define MSG_MAX_NAME_LENGTH 20
 
 /*flags of both client_to_server and server_to_client message*/
 #define MSG_EVERYONE	0
 #define MSG_SPECFIC	1
 #define MSG_LIST	2
-#define MSG_ANNOUCE	3
+#define MSG_ANNOUNCE	3
+#define MSG_LOGIN	4
 
 #define MSG_CLI_SRV_LENGTH 277
 struct msg_client_to_server{
 	unsigned char flags;//now only one flag, will adjust the size if flags are extended.
-	unsigned char name[20]; /*send_to or login_name*/
-	unsigned char content[MAX_CONTENT_LENGTH + 1];
+	unsigned char name[MSG_MAX_NAME_LENGTH + 1]; /*send_to or login_name*/
+	unsigned char content[MSG_MAX_CONTENT_LENGTH + 1];
 };
 
-#define MSG_SRV_CLI_LENGTH 
 struct msg_server_to_client{
 	unsigned char flags;//now only one flag.
-	unsigned char name[20]; /*send_to or online_user_num for the first char(byte).*/
+	unsigned char name[MSG_MAX_NAME_LENGTH + 1]; /*send_to or online_user_num for the first char(byte).*/
 	union{
-		unsigned char content[MAX_CONTENT_LENGTH + 1];
+		unsigned char content[MSG_MAX_CONTENT_LENGTH + 1];
 		unsigned char **list;
-	}
-}
+	};
+};
 
 #endif
