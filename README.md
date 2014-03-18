d5733074d1ec8b5ac875edfc88414f63778a6a6b
oubeichen <oubeichen@gmail.com>
2014-03-18 11:59:50 +0800

    Code clean (should change to pthread from fork).

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-This is the git repository for lab02 of computer network.
-lab02 is a experiment for a weather application using socket.
+This is the git repository for lab03 of computer network.
+lab03 is a experiment for a instant messenger using socket.
 
 client.c is the source of the client
 server.c is the source of the server
 
 make all: Compile server.c to server and client.c to client
-make test: Generate and run client(with local server 127.0.0.1, should work with "make srv")
-make cli: Generate and run client(with official server 114.212.191.33)
+make test: Generate and run client(with local server 127.0.0.1:6666, should work with "make srv")
+make cli: Generate and run client(with default server 127.0.0.1:6666, should work with "make srv")
 make srv: Generate and run server
+
--- server.c
+++ server.c
@@ -9,17 +9,13 @@
 #define MAXLINE 4096 /*max text line length*/
 #define SERV_PORT 6666 /*port*/
 #define LISTENNQ 8 /*maximum number of client connections*/
-#define MAXLEN_CITY 29 /*max length of city name*/
-#define MAXCITY_NUM 30 /*maximum number of cities*/
-#define SEND_SIZE 137/*length of receiving package's data*/
 
 int main(int argc, char **argv)
 {
-	int listenfd, connfd, n, citynum = 3, i;
+	int listenfd, connfd, n;
 	pid_t childpid;
 	socklen_t clilen;
 	unsigned char recvline[MAXLINE],sendline[MAXLINE];
-	char cities[MAXCITY_NUM][MAXLEN_CITY + 1] = {"nanjing", "beijing", "shanghai"};
 	struct sockaddr_in cliaddr, servaddr;
 	time_t now;
 	struct tm *date;
@@ -56,81 +52,7 @@ int main(int argc, char **argv)
 			//close listening socket
 			close(listenfd);
 
-			srand(time(NULL));/*Generate a random seed*/
-
 			for(; (n = recv(connfd, recvline, MAXLINE,0)) > 32;
-					send(connfd, sendline, SEND_SIZE,0), 
-					memset(recvline, 0, MAXLINE),memset(sendline, 0, MAXLINE)){//valid length
-				printf("Received a line.\n");
-
-				strncpy(sendline + 2, recvline + 2, MAXLEN_CITY);
-				
-				//always check city, in order to some unsafe query
-				printf("A client is querying city: %s.\n", sendline + 2);
-				for(i = 0;i < citynum;i++){
-					if(strcmp(sendline + 2, cities[i]) == 0)
-						break;
-				}
-				if(recvline[0] == 1)//validate city
-				{
-					if(i == citynum)//city not found
-					{
-						printf("City not found.\n");
-						sendline[0] = 4;
-					}else{
-						sendline[0] = 3;
-					}
-
-				}else if(recvline[0] == 2){//second menu
-					time(&now);
-					date = localtime(&now);
-					sendline[32] = (date->tm_year + 1900) / 0x100;
-					sendline[33] = (date->tm_year + 1900) % 0x100;
-					sendline[34] = date->tm_mon + 1;
-					sendline[35] = date->tm_mday;
-
-					if(i == citynum)//city not found,reply no such weather
-					{
-						printf("City not found.\n");
-						sendline[0] = 2;
-					}else{//valid city
-						if(recvline[1] == 1){//one day
-							int query_day = recvline[32];
-							sendline[1] = 0x41;
-							//generate a random weather
-							if(query_day >= 0 && query_day <= 5)//allowing 0~5
-							{
-								sendline[36] = recvline[32];
-								sendline[37] = rand() % 5;//weather
-								sendline[38] = rand() % 35;//temp 0~35
-								printf("Day%d 's weather generated.\n", query_day);
-								sendline[0] = 1;
-							}else{//invalid query or weather not found
-								printf("Weather not found.\n");
-								sendline[0] = 2;
-							}
-						}else if(recvline[1] == 2){//three days
-							sendline[1] = 0x42;
-							if(recvline[32] == 3){//only 3 is allowed
-								sendline[36] = 3;
-								sendline[37] = rand() % 5;//weather1
-								sendline[38] = rand() % 35;//temp1
-								sendline[39] = rand() % 5;//weather1
-								sendline[40] = rand() % 35;//temp1
-								sendline[41] = rand() % 5;//weather1
-								sendline[42] = rand() % 35;//temp1
-								printf("Three days' weather generated.\n");
-								sendline[0] = 1;
-							}else{//invalid query
-								sendline[0] = 2;
-							}
-						}else{//invalid query
-							sendline[0] = 2;
-						}
-					}
-				}else{
-					sendline[0] = 5;
-				}
 
 			}
 
