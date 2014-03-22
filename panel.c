d6fa8a715db22ebe87c9a1ed9c73f223233116af
oubeichen <oubeichen@gmail.com>
2014-03-22 13:07:07 +0800

    GUI code cleanup

--- panel.c
+++ panel.c
@@ -1,21 +1,18 @@
 #include <panel.h>
+#include <string.h>
 
 void init_wins(WINDOW **wins);
-void win_show(WINDOW *win, char *label, int label_color);
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color);
+void print_label(WINDOW **wins);
 
 int main()
 {	WINDOW *my_wins[2];
 	PANEL  *my_panels[2];
 	PANEL  *top;
-	int ch;
 
 	/* Initialize curses */
 	initscr();
 	start_color();
-	cbreak();
-	noecho();
-	keypad(stdscr, TRUE);
+	//cbreak();
 
 	/* Initialize all the colors */
 	init_pair(1, COLOR_RED, COLOR_BLACK);
@@ -25,29 +22,30 @@ int main()
 
 	init_wins(my_wins);
 	
-	/* Attach a panel to each window */ 	/* Order is bottom up */
-	my_panels[0] = new_panel(my_wins[0]); 	/* Push 0, order: stdscr-0 */
-	my_panels[1] = new_panel(my_wins[1]); 	/* Push 1, order: stdscr-0-1 */
+	/* Attach a panel to each window */ 
+	my_panels[0] = new_panel(my_wins[0]);
+	my_panels[1] = new_panel(my_wins[1]);
 
-	/* Set up the user pointers to the next panel */
-	set_panel_userptr(my_panels[0], my_panels[1]);
-	set_panel_userptr(my_panels[1], my_panels[0]);
-
-	/* Update the stacking order. 2nd panel will be on top */
+	/* Update the stacking order. */
 	update_panels();
 
 	/* Show it on the screen */
 	attron(COLOR_PAIR(4));
 	attroff(COLOR_PAIR(4));
 	doupdate();
+    refresh();
 
-	wmove(my_wins[1],2,1);
-	update_panels();
-	doupdate();
-
-	while((ch = getch()) != KEY_F(2))
+    /* Setup for chatting */
+    wsetscrreg(my_wins[0], 1, LINES - 5);
+    scrollok(my_wins[0], TRUE);
+	while(1)
 	{	
-		waddch(my_wins[1],ch);
+        wmove(my_wins[1], 1, 1);
+        char st[99];
+        wgetstr(my_wins[1], st);
+        wprintw(my_wins[0], "%s\n", st);
+        werase(my_wins[1]);
+        print_label(my_wins);
 		update_panels();
 		doupdate();
 	}
@@ -57,51 +55,27 @@ int main()
 
 /* Put all the windows */
 void init_wins(WINDOW **wins)
-{	int x, y, i;
-	char label[80];
-
-	y = 0;
-	x = 0;
-	wins[0] = newwin(LINES - 5, COLS, y, x);
-	sprintf(label, "Chat");
-	win_show(wins[0], label, 1);
-	y += LINES - 5;
-	wins[1] = newwin(4, COLS, y, x);
-	sprintf(label, "Input");
-	win_show(wins[1], label, 2);
-}
-
-/* Show the window with a border and a label */
-void win_show(WINDOW *win, char *label, int label_color)
-{	int startx, starty, height, width;
-
-	getbegyx(win, starty, startx);
-	getmaxyx(win, height, width);
-
-	box(win, 0, 0);
+{
+    char str[90];
+	wins[0] = newwin(LINES - 5, COLS, 0, 0);
+    wattron(wins[0], COLOR_PAIR(1));
+	wprintw(wins[0], "Help: \n\":list\" \t\tList online users.\n\":logout\" \t\tLog out.\n\"@someone <message>\" \tSend a private message to someone.\n");
+	wattroff(wins[0], COLOR_PAIR(1));
 	
-	print_in_middle(win, 1, 0, width, label, COLOR_PAIR(label_color));
+    wins[1] = newwin(4, COLS, LINES - 5, 0);
+	box(wins[1], 0, 0);
+	wattron(wins[1], COLOR_PAIR(2));
+
+    strcpy(str, "Welcome to oubeichen's chatroom.Please enter a name.");
+	mvwprintw(wins[1], 0, (COLS - strlen(str)) / 2, str);
+	wattroff(wins[1], COLOR_PAIR(2));
+    refresh();
 }
 
-void print_in_middle(WINDOW *win, int starty, int startx, int width, char *string, chtype color)
-{	int length, x, y;
-	float temp;
-
-	if(win == NULL)
-		win = stdscr;
-	getyx(win, y, x);
-	if(startx != 0)
-		x = startx;
-	if(starty != 0)
-		y = starty;
-	if(width == 0)
-		width = 80;
-
-	length = strlen(string);
-	temp = (width - length)/ 2;
-	x = startx + (int)temp;
-	wattron(win, color);
-	mvwprintw(win, y - 1, x, "%s", string);
-	wattroff(win, color);
-	refresh();
+void print_label(WINDOW **wins)
+{
+    box(wins[1], 0, 0);
+	wattron(wins[1], COLOR_PAIR(2));
+	mvwprintw(wins[1], 0, (COLS - 5) / 2, "Input");
+	wattroff(wins[1], COLOR_PAIR(2));
 }
